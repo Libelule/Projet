92f1df735043b7c4fbf03ac6eb1e1025512b3b37
corentinvzc <corentin.venzac@gmail.com>
2016-03-01 09:05:59 +0100

    appli

--- appli/js/libellus.js
+++ appli/js/libellus.js
@@ -1,4 +1,3 @@
-
 //-----------------  SCRIPT PRINCIPAL -------------------------
 var optionsMap;
 window.addEventListener("load", init);
@@ -8,46 +7,75 @@ var directionsDisplay
 
 //----------------- FONCTIONS ----------------------
 function init(eve) {
-//Si geoloc de l'utilisateur
 	if (navigator.geolocation)
-//Envoie dans la fonction
 	  navigator.geolocation.getCurrentPosition(actionSiSucces, actionSiPb);
 	else
 	  console.log("Votre navigateur ne prend pas en compte la géolocalisation");
-//Changement d'icône dans le menu
-		document.getElementById("location").innerHTML= 'location_disabled';
 	}
 
 function actionSiSucces(position){
-	//Changement d'icône dans le menu
-	document.getElementById("location").innerHTML= 'my_location';
-	//Coordonnée
   var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	//Option de la carte
+
   optionsMap = {
       zoom: 10,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.SATTELITE
 	};
-	//Affichage de la map
+
   map = new google.maps.Map(document.getElementById('zone'), optionsMap);
-	//Option du marqueur
+
   var optionsMarker = {
       position: latlng,
       map: map,
       title:"Vous etes ici"
 	};
-	//affichage du marqueur
+
 	var marker = new google.maps.Marker(optionsMarker);
 
   //----test affichage itineraires-------
     document.getElementById("l1").addEventListener("click", function(){
-    var ligne = document.getElementById("l1").value;
-    $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
-  });
+      var ligne = document.getElementById("l1").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+    document.getElementById("l2").addEventListener("click", function(){
+      var ligne = document.getElementById("l2").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+    document.getElementById("l3").addEventListener("click", function(){
+      var ligne = document.getElementById("l3").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+    document.getElementById("l4").addEventListener("click", function(){
+      var ligne = document.getElementById("l4").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+		document.getElementById("l5").addEventListener("click", function(){
+      var ligne = document.getElementById("l5").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+		document.getElementById("l6").addEventListener("click", function(){
+      var ligne = document.getElementById("l6").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+		document.getElementById("l7").addEventListener("click", function(){
+      var ligne = document.getElementById("l7").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+		document.getElementById("l8").addEventListener("click", function(){
+      var ligne = document.getElementById("l8").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+		document.getElementById("l9").addEventListener("click", function(){
+      var ligne = document.getElementById("l9").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
+		document.getElementById("l10").addEventListener("click", function(){
+      var ligne = document.getElementById("l10").value;
+      $.get("http://vsp149406.nfrance.com/~16_fernandes/Libellus/index.php?ligne="+ligne, null, afficher, "json");
+    });
 
 }
-//Affichage de ligne
+
 function afficher(dataJSON){
   directionsService = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer({
@@ -58,18 +86,21 @@ function afficher(dataJSON){
 
   directionsDisplay.setMap(map);
 
-
     directionsDisplay.polylineOptions.strokeColor = "green";
+
   var waypts = [];
   for(i=1; i<=8; i++){
     var latlng = new google.maps.LatLng(dataJSON[i].stop_lat, dataJSON[i].stop_lon)
     var point = {location : latlng, stopover : false};
     waypts.push(point);
   }
+
+  var taille = (dataJSON.length)-1;
+
   var directionsRequest = {
-    origin: new google.maps.LatLng(43.615264, 2.249882), //Bouriatte,
+    origin: new google.maps.LatLng(dataJSON[0].stop_lat, dataJSON[0].stop_lon),
     waypoints: waypts,
-    destination: new google.maps.LatLng(43.589893, 2.189078), //chartreuse,
+    destination: new google.maps.LatLng(dataJSON[taille].stop_lat, dataJSON[taille].stop_lon),
     travelMode: google.maps.DirectionsTravelMode.DRIVING,
     unitSystem: google.maps.UnitSystem.METRIC
   };
